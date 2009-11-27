aeb8b222fbc65e32ce588b14ed80b4fae7af5682
Mikael Tollefsen <mikael@tollefsen.biz>
2009-11-27 18:54:27 +0100

    Added user

---
+++ domain/src/main/java/com/exie/domain/User.java
@@ -0,0 +1,52 @@
+package com.exie.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author mikael
+ */
+public class User implements Serializable {
+
+    protected String name;
+    protected Date birthDate;
+
+    /**
+     * Get the value of birthDate
+     *
+     * @return the value of birthDate
+     */
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    /**
+     * Set the value of birthDate
+     *
+     * @param birthDate new value of birthDate
+     */
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+
+    /**
+     * Get the value of name
+     *
+     * @return the value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @param name new value of name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
